be894fd4c7d123778c57206f3dd0a1d23290d6eb
John DeSilva <desilvjo@umich.edu>
2017-08-28 09:49:41 -0400

    Add Organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828134842) do
+ActiveRecord::Schema.define(version: 20170828134933) do
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "repositories", force: :cascade do |t|
     t.string "name"
