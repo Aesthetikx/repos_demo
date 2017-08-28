a6b89b61925d0374c57c3280125153bc4324cf42
John DeSilva <desilvjo@umich.edu>
2017-08-28 10:12:27 -0400

    Implement User#repositories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+  def repositories
+    query = Repository
+            .joins("
+              LEFT JOIN organizations
+              ON repositories.owner_id = organizations.id
+              AND repositories.owner_type = 'Organization'
+            ") # Join repositories to their organization, if any
+            .joins("
+              LEFT JOIN memberships
+              ON memberships.organization_id = organizations.id
+            ") # So that we can select organizations we are members of
+            .references(
+              :memberships
+            ) # Relations passed to or must be structurally compatible
+
+    query.where(owner: self).or( # The ones I own directly
+      query.where(memberships: { user_id: id }) # Or ones in my organizations
+    )
+  end
 end
