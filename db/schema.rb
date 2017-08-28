1b31eb6c770ef7459e069c755f8edcea0b71e607
John DeSilva <desilvjo@umich.edu>
2017-08-28 09:50:15 -0400

    Add organization Memberships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828134933) do
+ActiveRecord::Schema.define(version: 20170828134958) do
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "organization_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organization_id"], name: "index_memberships_on_organization_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.string "name"
